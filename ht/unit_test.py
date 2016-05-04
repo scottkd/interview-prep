e82a0ca8f785a1e8ae4e0628dc6e267d689d87f0
Scott deRegt <scottderegt@gmail.com>
2016-05-04 14:19:07 -0700

    updated unit_test for proper requirements

--- ht/unit_test.py
+++ ht/unit_test.py
@@ -2,15 +2,21 @@ import sys
 
 # Read stdin and compare to pre-calculated result of test data.
 
-data = []
+data = ''
 chunk = sys.stdin.readline()
 while chunk:
-	data.append(chunk)
+	data += chunk
 	chunk = sys.stdin.readline()
-
-# Have to sort data for assert because multi-threading doesn't preserve order
-sorted_data = ''.join(sorted(data))
 
 # Not the prettiest testing structure, but does the job as described in the problem description. 
 # unittest suite would be created for production code.
-assert sorted_data == ',\narches,utah\nbadlands,north dakota\ncomma park,oregon\neverglades,miami\ntes\\\\nting,new\\\\nline\ntes\\\\ting,t\\\\tab\nyosemite,california\n', sorted_data
+assert data == """yosemite,california
+arches,utah
+badlands,north dakota
+everglades,miami
+testing,newline
+,
+testing,ttab
+comma park,oregon
+testing\\,back\\slash\\
+""", data
